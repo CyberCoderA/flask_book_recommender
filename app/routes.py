93b25e04a3bf7ee1708b2a06f6e61a6b09d82174
Adrian Pueblo <adrian.pueblo27@gmail.com>
2025-05-21 00:24:25 +0800

    Added base models.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,17 @@
 from flask import Blueprint, render_template
+from .models.LoginModel import LoginModel
 
 main = Blueprint('__main__', __name__)
 
 @main.route("/")
 def index():
     return render_template('index.html')
+
+@main.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginModel()
+
+    if form.validate_on_submit():
+        return "Successfully Logged In!"
+
+    return render_template('login.html', form=form)
