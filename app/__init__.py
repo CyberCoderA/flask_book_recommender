3501199f5c859ef05fe6905e4d387abb0abab63e
Adrian Pueblo <adrian.pueblo27@gmail.com>
2025-05-22 20:59:30 +0800

    Created database file.

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import DevelopmentConfig
 
+db = SQLAlchemy()
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(DevelopmentConfig)
-
-    db = SQLAlchemy()
     db.init_app(app)
-    
+
     app.register_blueprint(main)
+
+    with app.app_context():
+        db.create_all()
 
     return app
--- app/models/BookModel.py
+++ app/models/BookModel.py
@@ -1,2 +1,15 @@
+from app import db
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
+
+class Books(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    genre = db.Column(db.String(255), nullable=False)
+    publication_year = db.Column(db.DateTime, default=datetime.utcnow)
+    cover = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
