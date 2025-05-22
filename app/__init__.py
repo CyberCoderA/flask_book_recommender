03b6ebeba0e802a399623cee3726c2f1fd3c51b5
Adrian Pueblo <adrian.pueblo27@gmail.com>
2025-05-22 19:31:03 +0800

    Set up database and book model.

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from .routes import main
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from config import DevelopmentConfig
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(DevelopmentConfig)
+
+    db = SQLAlchemy()
+    db.init_app(app)
     
     app.register_blueprint(main)
 
