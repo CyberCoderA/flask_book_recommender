3b8061e3a67f700d31a2ee7f624820bf9b65b34b
Adrian Pueblo <adrian.pueblo27@gmail.com>
2025-05-23 14:12:18 +0800

    Fixed to accomodate multi-genre books.

--- app/routes.py
+++ app/routes.py
@@ -33,15 +33,22 @@ def register():
     return render_template('register.html', form=form)
 
 def process_recommendation(preffered_genre):
-    recommended_books = ""
-
     data = {
         'Title': ['A Game of Thrones', 'Lucifer', 'Rain in España', 'Outlander', 'Replay'],
-        'Genre': ['Fantasy', 'Fiction', 'Romance', 'Historical', 'Fiction']
+        'Genre': [['Fantasy'], ['Fiction'], ['Romance'], ['Romance','Historical'], ['Fiction']]
     }
 
-    for genre in preffered_genre:
-        df = pd.DataFrame(data)
-        recommended_books += str(df[df['Genre'] == genre]['Title'].tolist())
+    df = pd.DataFrame(data)
+    recommended_books = ""
+    num_rows = df.shape[0]
+
+    for i in range(0, num_rows):
+        for j in df['Genre'].loc[i]:
+            for genre in preffered_genre:
+                if(j == genre):
+                    recommended_books += f"{df['Title'].loc[i]} | "
+
+    # for genre in preffered_genre:
+    #     recommended_books += str(df[df['Genre'] == genre]['Title'].tolist())
     
     return recommended_books if recommended_books else ["No books found with selected genres!"]
